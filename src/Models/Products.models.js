e05fc04f8482efad5e2b476bc7e1c72a197489a2
blankitolv <lucasvega2@gmail.com>
2025-06-06 21:02:17 -0300

    delete unused code

--- src/Models/Products.models.js
+++ src/Models/Products.models.js
@@ -1,20 +1,4 @@
-// terceros
-import { v4 as uuidv4 } from "uuid";
-
-// nativas
-import fs from "fs/promises";
-import path from "path";
-
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// const DB_FILE = path.join(__dirname,"..","database", "productos.json");
-
 import mongoose from "mongoose";
-
 
 const productSchema = new mongoose.Schema({
   title: String,
